e008b05caac2bdabd6e848490515055b90dee007
Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>
2023-04-24 13:54:33 -0300

    M 02 (#2)
    * F 01 (#1)
    
    * v1
    
    * v2
    
    * v3
    
    * v3
    
    * v4
    
    * formato
    
    * fix formato
    
    * accesibilidad en los alt
    
    * M-02

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: "jit",
+	purge: [
+		"./src/**/*.{js,jsx,ts,tsx}", //esto también es una forma de englobar subdirectorios
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ["Inter", `sans-serif`],
+				montserrat: ["Montserrat", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+			backgroundImage: {
+				Footer: "url('./assets/images/Footer.png')",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
