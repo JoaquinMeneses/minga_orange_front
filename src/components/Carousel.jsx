426ab4ac34960774a32814726efbd6f0834399bf
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-26 15:38:14 -0300

    fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,33 +1,4 @@
 import React, { useState, useEffect } from 'react';
-<<<<<<< HEAD
-
-export default function Carousel({ images }) {
-    const [activeIndex, setActiveIndex] = useState(0);
-    useEffect(() => {
-        // Función para cambiar a la siguiente imagen
-        const slideNext = () => {
-            setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-        };
-        // Inicia el cambio automático de imagen cada 3 segundos
-        const interval = setInterval(slideNext, 3000);
-        // Limpia el intervalo cuando el componente se desmonta
-        return () => {
-            clearInterval(interval);
-        };
-    }, [images]);
-    return (
-        <div>
-            {images.map((image, index) => (
-                <img
-                    key={index}
-                    src={image.src}
-                    alt={image.alt}
-                    className={`absolute h-full w-[100vw] md:w-[40%] md:ml-[8%] object-cover filter brightness-75 md:filter md:brightness-100 ${activeIndex === index ? 'opacity-100' : 'opacity-0'}`}
-                />
-            ))}
-        </div>
-    );
-=======
 import axios from 'axios';
 import apiUrl from '../../api';
 
@@ -58,5 +29,4 @@ export default function Carousel() {
             <img className="hidden sm:flex h-full w-[100vw] md:w-[40%] md:ml-[8%] object-cover filter brightness-50 md:filter md:brightness-100" src={images[counter]?.cover_photo} alt="" />
         </div>
     )
->>>>>>> ca9dd8ecbe7614df1f054b651574b2322e5a1617
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -104,15 +104,6 @@ export default function Footer() {
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
-<<<<<<< HEAD
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6 mx-[0.5rem]"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-=======
                             strokeWidth="1.5"
                             stroke="currentColor"
                             className="w-6 h-6 mx-[0.5rem]"
@@ -120,7 +111,6 @@ export default function Footer() {
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
->>>>>>> ca9dd8ecbe7614df1f054b651574b2322e5a1617
                                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                             />
                         </svg>
