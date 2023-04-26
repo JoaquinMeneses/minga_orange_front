4a1cd48400e8c34ba65771c7d13257550bfc3d26
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-04-25 22:21:32 -0300

    correccion menu burger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../assets/images/Logo.png";
 
-export default function NavBar() {
+function NavBar() {
     const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
 
     const handleMenuClick = () => {
@@ -63,3 +63,5 @@ export default function NavBar() {
         </nav>
     );
 }
+
+export default NavBar
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from "../components/Footer";
-import NavBar from "../components/NavBar";
+import Footer from "../components/Footer.jsx";
+import NavBar from "../components/NavBar.jsx";
 
 export default function Main({ children }) {
     return (
